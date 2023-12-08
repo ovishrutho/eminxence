8db724c6bb081ea7c2cf83e8202c0cd3c9ffb8fe
eminxence <vishruth.com@gmail.com>
2023-12-07 21:41:28 -0800

    eminxence

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <a href="/">emin<b>X</b>ence</a>
+      </header>
+
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
